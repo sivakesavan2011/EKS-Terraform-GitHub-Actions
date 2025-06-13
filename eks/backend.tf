18d0e5ffc561a63b9e936f0c9c578a1a965ea5ba
sivakesavan2011 <110766078+sivakesavan2011@users.noreply.github.com>
2025-06-13 13:50:04 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "dev-1234567-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "Lock-File"
     encrypt        = true
   }
 }
