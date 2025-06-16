7099dc5e33d595fec458674ece5517f8e93453cb
sivakesavan2011 <110766078+sivakesavan2011@users.noreply.github.com>
2025-06-16 12:25:27 +0530

    Update iam.tf
    eks-odic name change to ''-p

--- module/iam.tf
+++ module/iam.tf
@@ -71,7 +71,7 @@ resource "aws_iam_role_policy_attachment" "eks-AmazonEBSCSIDriverPolicy" {
 # OIDC
 resource "aws_iam_role" "eks_oidc" {
   assume_role_policy = data.aws_iam_policy_document.eks_oidc_assume_role_policy.json
-  name               = "eks-oidc"
+  name               = "eks-oidc-p"
 }
 
 resource "aws_iam_policy" "eks-oidc-policy" {
